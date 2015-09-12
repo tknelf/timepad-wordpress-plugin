7150001df4614404072a9f6f9db434c3de815434
Igor Sazonov <sovletig@yandex.ru>
2015-09-12 13:03:13 +0300

    anonymous function use

--- lib/setup.admin.php
+++ lib/setup.admin.php
@@ -65,7 +65,7 @@ if ( ! class_exists( 'TimepadEvents_Setup_Admin' ) ) :
             }
             
             //init TimePadEvents cron functionality
-            add_action( 'timepad_cron', function() {
+            add_action( 'timepad_cron', function() use ( $this ) {
                 if ( $this->_data['autoimport'] ) {
                     require_once(TIMEPADEVENTS_PLUGIN_ABS_PATH . 'lib/admin/menu/settings/tabs/general.php');
                     TimepadEvents_Admin_Settings_General::getInstance()->post_events($this->_data['current_organization_id']);
